15abdadaada7e6d55be75570ef77f320373d8339
Zeyuan Tang <zeyuan.tang@outlook.com>
2018-12-22 21:20:15 +0100

    non-optimised DBSCAN

--- week14/dbscan.py
+++ week14/dbscan.py
@@ -7,40 +7,35 @@ def get_neighbors(data, p, eps=1.5):
         if norm(x - p) < eps:
             if not np.array_equal(x, p):
                 neighbors.append(x)
-    return np.array(neighbors)
+    return neighbors
+
+def assign_labels(data, labels, p, k, eps, minPts):
+    p_neighbors = get_neighbors(data, p, eps)
+    i = 0
+    while i < len(p_neighbors):
+        pn = p_neighbors[i]
+        index_pn = np.where(np.all(data==pn, axis=1))[0][0]
+        if labels[index_pn] == 0:
+            labels[index_pn] = k
+        if labels[index_pn] == None:
+            labels[index_pn] = k
+            new_neighbors = get_neighbors(data, pn, eps)
+            if len(new_neighbors) >= minPts:
+                p_neighbors = np.vstack((p_neighbors, new_neighbors))
+        i += 1
 
 def my_DBSCAN(data, eps=1.5, minPts=3):
-    c = 0
-    labels = {tuple(o): None for o in data}
-    for o in data:
-        o = tuple(o)
-        if labels[o] != None: 
-            continue
-        
-        neighbors = get_neighbors(data, o, eps)
-        if len(neighbors) < minPts:
-            labels[o] = c # noise
-            continue
-        
-        c += 1
-        labels[o] = c
-        for x in neighbors:
-            x = tuple(x)
-            if labels[x] == 0:
-                labels[x] = c
-            elif labels[x] != None:
-                continue
+    labels = [None] * len(data)
+    k = 1 # index for cluster label
+    for index, o in enumerate(data):
+        # o: object
+        if labels[index] == None: # not classified object
+            o_neighbors = get_neighbors(data, o, eps)
+            if len(o_neighbors) >= minPts: # o is a core object
+                assign_labels(data, labels, o, k, eps, minPts)
+                k += 1
             else:
-                labels[x] = c
-                new_neighbors = get_neighbors(data, x, eps)
-                if len(new_neighbors) >= minPts:
-                    i = 0
-                    while i < len(neighbors):
-                        n = neighbors[i]
-                        if not np.any(neighbors[:,0] == n):
-                            print(n)
-                            neighbors = np.append(neighbors, n, axis=0)
-
+                labels[index] = 0 # o is not a core object, noise
     return labels
 
 
@@ -63,9 +58,15 @@ data = np.array([[1,3],
 [4,2],
 [4,5]])
 
-print(get_neighbors(data, data[0]))
-for key, value in my_DBSCAN(data).items():
-    print(key, value)
-plt.plot(data[:,0], data[:,1], 'bo')
+labels = my_DBSCAN(data, 1.2, 2)
+n_labels = len(np.unique(labels))
+print(labels)
+colors = ['C{}'.format(i) for i in range(n_labels)]
+
+for l, c in zip(np.arange(n_labels), colors):
+    x = data[np.where(labels == l)][:, 0]
+    y = data[np.where(labels == l)][:, 1]
+    print(x)
+    plt.plot(x, y, 'o', color=c)
 plt.show()
 
